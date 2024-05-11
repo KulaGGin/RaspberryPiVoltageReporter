1b2f23d857666b7e26f9690c41e00b8ea7fc6a8c
DESKTOP-PCQMQK8\Kulagin <sergeant.coolagin@gmail.com>
2024-05-11 12:45:57 +0300

    Update project.

--- SensorsConfiguration.py
+++ SensorsConfiguration.py
@@ -1,5 +1,5 @@
 VoltageSensorInputVoltageMinMax = [0, 25]
 VoltageSensorOutputVoltageMinMax = [0, 5]
 
-CurrentSensorInputCurrentMinMax = [0, 220]
+CurrentSensorInputCurrentMinMax = [0, 200]
 CurrentSensorOutputCurrentMinMax = [0, 5]
